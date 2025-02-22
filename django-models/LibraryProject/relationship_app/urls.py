e09bdfd8fef4b70ff546b31739346c9f3e237d11
chris <127218220+chrisisaboke01@users.noreply.github.com>
2025-02-22 13:37:45 +0300

    Delete django-models/LibraryProject/relationship_app/urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.contrib.auth import views as auth_views
-from . import views
-
-urlpatterns = [
-    path('register/', views.register, name='register'),
-    path('login/', views.user_login, name='login'),
-    path('logout/', views.user_logout, name='logout'),
-]
